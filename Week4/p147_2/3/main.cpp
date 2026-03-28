cd05aa3e62120b3a849677a623ca69a63e2ad8bb
Choi Woong <yyamoo.oo@gmail.com>
2026-03-28 23:11:20 +0900

    확인용

--- Week4/p147_2/3/main.cpp
+++ Week4/p147_2/3/main.cpp
@@ -2,15 +2,15 @@
 
 #include <iostream>
 #include "3.hpp."
-
+//using namespace std; 이거쓰면 cin >> ? cout << ? 시용가능
 int main()
 {
     std::string s;
 
-    std::cout << "숫자를 입력하세요: ";
+    std::cout << "숫자를 입력하세요:";
     std::cin >> s;
 
-    std::cout << "역순: ";
+    std::cout << "역순:";
     std::cout << reverse(s) << std::endl;
 
     return 0;
